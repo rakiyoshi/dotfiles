5e5147da0e891403e4276d03d85644396202a151
Ryoh Akiyoshi <ryo614@gmail.com>
2020-10-02 11:01:03 +0900

    Update setup script

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+cd ${HOME}
+
+# nvim
 mkdir -p ~/.config
 cd ~/.config
 ln -s ~/.dotfiles/nvim .
+cd ${HOME}
+
+# env
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+curl -sfL https://direnv.net/install.sh | bash
+
+# git
+ln -s ~/.dotfiles/gitconfig .gitconfig
+ln -s ~/.dotfiles/gitignore_global .gitignore_global
+
+# zsh
+ln -s ~/.dotfiles/zshrc .zshrc
